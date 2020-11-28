580d655c1065525cb38a4204b2d6c2d25ba361bc
SergeyVishnevskiy <70287472+SergeyVishnevskiy@users.noreply.github.com>
2020-11-28 21:21:00 +0200

    task-02 done

--- JS/task-02.js
+++ JS/task-02.js
@@ -1,18 +1,28 @@
 // Задание 2
+//======================decision=================\\
+const ingredients = [
+  "Картошка",
+  "Грибы",
+  "Чеснок",
+  "Помидоры",
+  "Зелень",
+  "Приправы",
+];
+const itemsArray = ingredients.forEach((item) => {
+  const listItem = document.createElement("li");
+  listItem.textContent = item;
+  const ingredientsListRef = document.querySelector("#ingredients");
+  ingredientsListRef.append(listItem);
+});
+
+//================================================\\
+
 // В HTML есть пустой список ul#ingredients.
 
 {
   /* <ul id="ingredients"></ul>
 В JS есть массив строк.
 
-const ingredients = [
-  'Картошка',
-  'Грибы',
-  'Чеснок',
-  'Помидоры',
-  'Зелень',
-  'Приправы',
-];
 Напиши скрипт, который для каждого элемента массива
 ingredients создаст отдельный li,
   после чего вставит все li за одну операцию
