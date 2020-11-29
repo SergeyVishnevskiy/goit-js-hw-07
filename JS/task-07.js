c619b85096de4ec822ade9f9c1f40d22422a4716
SergeyVishnevskiy <70287472+SergeyVishnevskiy@users.noreply.github.com>
2020-11-29 21:02:06 +0200

    task-07 done

--- JS/task-07.js
+++ JS/task-07.js
@@ -1,8 +1,37 @@
 // Задание 7
+//======================decision=================\\
+const fontSizeControlRef = document.querySelector("#font-size-control");
+const textRef = document.querySelector("#text");
+const changesFontSize = (event) =>
+  (textRef.style.fontSize = event.currentTarget.value + "px");
+
+fontSizeControlRef.addEventListener("input", changesFontSize);
+//================================================\\
+
+//=====variant gradation========\\
+// function getRandomInt(min, max) {
+//   return Math.floor(Math.random() * (max - min)) + min;
+// }
+
+// function gradation() {
+//   textRef.style.color =
+//     "rgb(" +
+//     getRandomInt(0, 255) +
+//     ", " +
+//     getRandomInt(0, 255) +
+//     ", " +
+//     getRandomInt(0, 255) +
+//     ")";
+// }
+// fontSizeControlRef.addEventListener("input", gradation);
+//================================================\\
+
 // Напиши скрипт, который реагирует на изменение значения input#font - size - control
 // (событие input) и изменяет инлайн - стиль span#text обновляя свойство font - size.
 // В результате при перетаскивании ползунка будет меняться размер текста.
 
-// <input id="font-size-control" type="range" />
-// <br />
-// <span id="text">Абракадабра!</span>
+{
+  /* <input id="font-size-control" type="range" />
+<br />
+<span id="text">Абракадабра!</span> */
+}
