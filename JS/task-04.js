782103f283fdff1a00a11477daf418481dbaec51
SergeyVishnevskiy <70287472+SergeyVishnevskiy@users.noreply.github.com>
2020-11-30 17:31:28 +0200

    task-04 done

--- JS/task-04.js
+++ JS/task-04.js
@@ -2,6 +2,8 @@
 //======================decision=================\\
 let counterValue = 0;
 const valueRef = document.querySelector("#value");
+// const decrementBtn = document.querySelector('[data-action="decrement"]');
+// const incrementBtn = document.querySelector('[data-action="increment"]');
 const decrementBtn = document.querySelector('button[data-action="decrement"]');
 const incrementBtn = document.querySelector('button[data-action="increment"]');
 const decrement = () => {
