17fc34a00d5e981b8ecd0d4c3a6f802071aa90dc
SergeyVishnevskiy <70287472+SergeyVishnevskiy@users.noreply.github.com>
2020-12-01 16:29:39 +0200

    task-08

--- JS/task-08.js
+++ JS/task-08.js
@@ -1,4 +1,51 @@
 // Задание 8 - дополнительное, выполнять не обязательно
+//======================decision=================\\
+let count;
+const boxRefs = {
+  div: document.getElementById("boxes"),
+  input: document.querySelector('[type="number"]'),
+  renderButton: document.querySelector('button[data-action="render"]'),
+  destroyButton: document.querySelector('button[data-action="destroy"]'),
+};
+boxRefs.input.addEventListener("change", (event) => {
+  count = event.target.value;
+  return count;
+});
+boxRefs.renderButton.addEventListener("click", (event) => {
+  createBox(count);
+});
+boxRefs.destroyButton.addEventListener("click", () => {
+  destroyBoxes();
+});
+function createBox(amount = 6) {
+  let size = 30;
+  let colorBox = document.createElement("div");
+  colorBox.style.width = size + "px";
+  colorBox.style.height = size + "px";
+  for (let i = 1; i <= amount; i += 1) {
+    size += 10;
+    let colorBox = document.createElement("div");
+    colorBox.style.width = size + "px";
+    colorBox.style.height = size + "px";
+    let color = createRgb();
+    colorBox.style.backgroundColor = color;
+    boxRefs.div.append(colorBox);
+  }
+}
+
+function createRgb() {
+  let red = Math.round(Math.random() * 255);
+  let green = Math.round(Math.random() * 255);
+  let blue = Math.round(Math.random() * 255);
+  return `rgb(${red}, ${green}, ${blue})`;
+}
+function destroyBoxes() {
+  boxRefs.div.innerHTML = "";
+  boxRefs.input.value = "";
+}
+
+//
+
 // Напиши скрипт создания и очистки коллекции элементов.
 // Пользователь вводит количество элементов в input и нажимает кнопку Создать,
 // после чего рендерится коллекция.При нажатии на кнопку Очистить, коллекция элементов очищается.
